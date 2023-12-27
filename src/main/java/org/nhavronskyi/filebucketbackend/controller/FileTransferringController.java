18280926ed54a4184eb037af52f739560a3c2309
nazar <nhavronskyi@gmail.com>
2023-12-27 20:30:49 +0100

    receive files from clients

--- src/main/java/org/nhavronskyi/filebucketbackend/controller/FileTransferringController.java
+++ src/main/java/org/nhavronskyi/filebucketbackend/controller/FileTransferringController.java
@@ -2,26 +2,36 @@ package org.nhavronskyi.filebucketbackend.controller;
 
 import lombok.SneakyThrows;
 import org.nhavronskyi.filebucketbackend.enums.SavingStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.nio.file.Path;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 
 @RestController
 @RequestMapping("files")
 public class FileTransferringController {
 
     @GetMapping
-    public String test(){
+    public String test() {
         return "hello";
     }
 
     @SneakyThrows
-    @PostMapping("save")
-    public SavingStatus saveFile(@RequestParam MultipartFile file){
-        if (!file.isEmpty()){
-            file.transferTo(Path.of("src/main/java/org/nhavronskyi/filebucketbackend/files"));
+    @GetMapping(value = "save", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public SavingStatus saveFile(@RequestParam("file") MultipartFile file) {
+        if (!file.isEmpty()) {
+            File newFile = new File("src/main/java/org/nhavronskyi/filebucketbackend/files/" + file.getOriginalFilename());
+            try (OutputStream outStream = new FileOutputStream(newFile)) {
+                outStream.write(file.getBytes());
+            }
+            return SavingStatus.SAVED;
         }
-        return SavingStatus.SAVED;
+        return SavingStatus.ERROR;
     }
 }
